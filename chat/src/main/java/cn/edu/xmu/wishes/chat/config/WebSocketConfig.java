05d6c0da5fe9700fabbff3fc0d642f14dcff0c3c
Bwly999 <438225686@qq.com>
2022-02-07 12:52:42 +0800

    websocket聊天实验成功

--- chat/src/main/java/cn/edu/xmu/wishes/chat/config/WebSocketConfig.java
+++ chat/src/main/java/cn/edu/xmu/wishes/chat/config/WebSocketConfig.java
@@ -1,24 +1,74 @@
 package cn.edu.xmu.wishes.chat.config;
 
+import cn.edu.xmu.wishes.chat.model.WebsocketUserVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private SimpUserRegistry userRegistry;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/endpointChat")
-                .setAllowedOrigins("*");
+                .setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
-
+        registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/private");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    System.out.println("连接success");
+                    Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
+                    if (raw instanceof Map) {
+                        Object name = ((Map) raw).get("user");
+                        if (name instanceof List) {
+                            String id = ((List) name).get(0).toString();
+                            //设置当前访问器的认证用户
+                            accessor.setUser(new WebsocketUserVo(id));
+                        }
+                    }
+                } else if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
+                    //点击断开连接，这里会执行两次，第二次执行的时候，message.getHeaders.size()=5,第一次是6。直接关闭浏览器，只会执行一次，size是5。
+                    System.out.println("断开连接");
+                    WebsocketUserVo vo = (WebsocketUserVo) message.getHeaders().get(SimpMessageHeaderAccessor.USER_HEADER);
+
+                    //  如果同时发生两个连接，只有都断开才能叫做不在线
+//                    if (message.getHeaders().size() == 5&&StringUtils.isBlank(userRegistry.getUser(vo.getName()))) {
+//                        webSocketServ.removeOnlineUser(vo.getName());
+//                    }
+                }
+                return message;
+            }
+        });
     }
 }
--- chat/src/main/java/cn/edu/xmu/wishes/chat/controller/ChatController.java
+++ chat/src/main/java/cn/edu/xmu/wishes/chat/controller/ChatController.java
@@ -5,19 +5,29 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ChatController {
+    @Autowired
     private SimpMessagingTemplate brokerMessagingTemplate;
 
     @MessageMapping("/chat/private")
-    public Chat privateChat(Chat content) throws JsonProcessingException {
+    public Chat privateChat(@Payload Chat content) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         System.out.println(objectMapper.writeValueAsString(content));
 
         this.brokerMessagingTemplate.convertAndSendToUser(content.getReceiver(), "/topic/chat", content);
         return content;
+    }
+
+    @MessageMapping("/chat/public") //这里是客户端发送消息对应的路径，等于configureMessageBroker中配置的setApplicationDestinationPrefixes + 这路径即 /app/sendPublicMessage
+    @SendTo("/topic/chat") //也可以使用 messagingTemplate.convertAndSend(); 推送
+    public Chat sendPublicMessage(@Payload Chat chatMessage) {
+        return chatMessage;
     }
 }
