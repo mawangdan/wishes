30b64e84d67a9ca63385d321030173f95b91b558
Bwly999 <438225686@qq.com>
2022-06-21 11:12:56 +0800

    add swagger request header config

--- core/src/main/java/cn/edu/xmu/wishes/core/config/SwaggerConfig.java
+++ core/src/main/java/cn/edu/xmu/wishes/core/config/SwaggerConfig.java
@@ -2,13 +2,19 @@ package cn.edu.xmu.wishes.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -19,8 +25,25 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalRequestParameters(List.of(authorizationParameter()));
     }
+
+    /**
+     * 添加head参数配置
+     */
+    private RequestParameter authorizationParameter() {
+        RequestParameterBuilder tokenBuilder = new RequestParameterBuilder();
+        tokenBuilder
+                .name("Authorization")
+                .description("access_token")
+                .required(false)
+                .in("header")
+                .accepts(Collections.singleton(MediaType.APPLICATION_JSON))
+                .build();
+        return tokenBuilder.build();
+    }
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 解决静态资源无法访问
