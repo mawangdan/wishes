e53de7bbb65ed152f7d9279c14d96f9f06ce1f81
Bwly999 <438225686@qq.com>
2022-06-23 19:53:48 +0800

    fix mainClass not found

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup java -jar ./gateway/target/gateway-1.0-SNAPSHOT.jar >> ./gateway/logs/start.out &
-nohup java -jar ./task/target/task-1.0-SNAPSHOT.jar >> ./task/logs/start.out &
+nohup java -jar ./gateway/target/gateway-1.0-SNAPSHOT.jar >> ./logs/gateway.out &
+nohup java -jar ./task/target/task-1.0-SNAPSHOT.jar >> ./logs/task.out &
