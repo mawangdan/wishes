acca3d470d765025878cad6888ad736ebdc8ce22
mawangdan <mawangdan@foxmail.com>
2022-06-29 20:03:27 +0800

    ocrApi完成

--- task/src/main/java/cn/edu/xmu/wishes/TaskApplication.java
+++ task/src/main/java/cn/edu/xmu/wishes/TaskApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = {"cn.edu.xmu.wishes.core", "cn.edu.xmu.wishes.task", "cn.edu.xmu.wishes.user","cn.edu.xmu.wishes.news"})
+@SpringBootApplication(scanBasePackages = {"cn.edu.xmu.wishes.core", "cn.edu.xmu.wishes.task", "cn.edu.xmu.wishes.user","cn.edu.xmu.wishes.news","cn.edu.xmu.wishes.cloud","cn.edu.xmu.wishes.util"})
 @MapperScan({"cn.edu.xmu.wishes.task.mapper", "cn.edu.xmu.wishes.user.mapper","cn.edu.xmu.wishes.news.mapper"})
 @EnableConfigurationProperties
 public class TaskApplication {
--- task/src/main/java/cn/edu/xmu/wishes/util/test.java
+++ task/src/main/java/cn/edu/xmu/wishes/util/test.java
@@ -10,8 +10,7 @@ import com.chinamobile.cmss.sdk.ocr.request.IECloudRequest;
 import com.chinamobile.cmss.sdk.ocr.request.ocr.OcrRequestFactory;
 import org.apache.commons.codec.binary.Base64;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -20,5 +19,20 @@ import java.util.List;
 public class test {
     public static void main(String[] args) {
         //用户鉴权对象
+        File file = new File("D:\\dasanXia\\wishes\\task\\src\\main\\java\\cn\\edu\\xmu\\wishes\\util\\2.jpg");
+        try {
+            byte[] fileByte = Files.readAllBytes(file.toPath());
+            System.out.println();
+
+            String path = "D:\\dasanXia\\wishes\\task\\src\\main\\java\\cn\\edu\\xmu\\wishes\\util\\1.txt";
+            String word =Base64.encodeBase64String(fileByte);
+            BufferedWriter out = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(path,true)));
+            out.write(word);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
