173c6b68cfc836f0a87b57d14c18ed8bfdd8234e
Bwly999 <438225686@qq.com>
2022-07-25 17:24:07 +0800

    change database config

--- sql/wishes-data.sql
+++
@@ -1,6 +0,0 @@
--- ----------------------------
--- Records of wishes_task_type
--- ----------------------------
-INSERT INTO `wishes_task_type` VALUES (1, '跑腿', 0, 0, NULL, NULL, NULL, NULL, '2022-03-15 20:45:27', NULL);
-INSERT INTO `wishes_task_type` VALUES (2, '解答', 0, 0, NULL, NULL, NULL, NULL, '2022-03-15 20:45:27', NULL);
-INSERT INTO `wishes_task_type` VALUES (3, '交易', 0, 0, NULL, NULL, NULL, NULL, '2022-03-15 20:45:27', NULL);
